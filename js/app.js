975599213b6c351a2cf60eb2b0f414a7c461670d
chanteloatman <31936830+chanteloatman@users.noreply.github.com>
2017-11-22 11:37:54 -0500

    start to create js for controls and gallery

--- js/app.js
+++ js/app.js
@@ -1,2 +1,41 @@
 //Do not under any circumstances remove this line!
 $(document).foundation()
+
+(function(){
+	"use strict";
+	
+	
+	//VARIABLES
+	//......................Video Controls........................
+    var playButton = document.querySelector("#playButton");
+	var pauseButton = document.querySelector("#pauseButton");
+	var fullScreenButton = document.querySelector("#fullScreen");
+    //.....................Gallery Buttons........................
+	var previousButton = deocument.querySelector("#previousButton");
+	var nextButton = deocument.querySelector("#nextButton");
+	
+	//FUNCTIONS
+	//......................Video Controls........................
+	function playVideo(){
+		playButton.playButton();
+	}
+	function pauseVideo(){
+		pauseButton.pauseButton();
+	}
+	function fullScreen(){
+		//insert code for full screen function here
+	}
+	//.....................Gallery Buttons........................
+	function previousImage(){
+	//insert code for previous function here
+	}
+	function nextImage(){
+	//insert code for previous function here
+	}
+	
+	//LISTENERS
+	playButton.addEventListener("click", playVideo, false);
+	pauseButton.addEventListner("click", pauseVideo, false);
+	
+	
+})();
