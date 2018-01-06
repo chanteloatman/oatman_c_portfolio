b580811439fe25b29c6917ec4b991a1ac1737b10
chanteloatman <31936830+chanteloatman@users.noreply.github.com>
2018-01-05 23:45:05 -0500

    add homepage functionality, hide shows, design tweaks

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// JavaScript Document
+
+(function(){
+	"use strict";
+	
+	//Variables
+	var photoServ = document.querySelector("#photoServicesDescS");
+	var photoServButton = document.querySelector("#photoServiceButton");
+	
+	var graphicServ = document.querySelector("#graphicServicesDescS");
+	var graphicServButton = document.querySelector("#graphicServiceButton");
+	
+	var brandServ = document.querySelector("#brandServicesDescS");
+	var brandServButton = document.querySelector("#brandServiceButton");
+	
+	//Functions
+	//Show/Hide Photo Services
+	function hideShowPhotoServ(){
+		console.log("hide show working");
+		//display none
+		if(photoServ.style.display === 'block'){
+			photoServ.style.display = 'none';}
+		//display block
+		else {photoServ.style.display = 'block';}
+	}
+	//Show/Hide Graphic Services
+	function hideShowGraphicServ(){
+		console.log("hide show working");
+		//display none
+		if(graphicServ.style.display === 'block'){
+			graphicServ.style.display = 'none';}
+		//display block
+		else {graphicServ.style.display = 'block';}
+	}
+	//Show/Hide Brand Services
+	function hideShowBrandServ(){
+		console.log("hide show working");
+		//display none
+		if(brandServ.style.display === 'block'){
+			brandServ.style.display = 'none';}
+		//display block
+		else {brandServ.style.display = 'block';}
+	}
+	
+	//Listeners
+	photoServButton.addEventListener("click", hideShowPhotoServ, false);
+	graphicServButton.addEventListener("click", hideShowGraphicServ, false);
+	brandServButton.addEventListener("click", hideShowBrandServ, false);
+	
+	})();
